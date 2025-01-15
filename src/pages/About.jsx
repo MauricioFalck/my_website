8ea6142ce6b9a2b7623d31b3b145c2245a0a01ef
MauricioFalck <mcfalck@gmail.com>
2025-01-15 17:44:34 -0300

    initial changes on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,9 +13,13 @@ function About() {
         </div>
         <div className="md:w-1/2 mt-6 md:mt-0 md:ml-8">
           <h3 className="text-gray-600">AI Enthusiast | Passionate Solution Creator for Customer Success</h3>
-          <h2 className="text-2xl font-semibold mt-2">Mauricio <span className='text-gray-500'>Javier Cisternas </span>Falck</h2>
+          <h2 className="text-2xl font-semibold mt-2">Mauricio Falck</h2>
           <p className="mt-4">
-            I am a passionate developer with experience in...
+            With a career spanning nearly four decades in the IT industry, I am a seasoned professional known for my expertise in telecommunications systems, OSS/BSS, and large-scale systems architecture. I hold degrees in Electrical Engineering (1997) and Computer Science (2024), and I have made significant contributions to global giants like IBM, HP, Ericsson, and Amdocs.
+          </p>
+          <br />
+          <p>
+            Today, as one of the founders of Greatminds Inc., I am dedicated to delivering innovative AI solutions tailored to customer management needs. With a blend of deep technical knowledge and strategic vision, I am committed to driving success and innovation in the digital age.
           </p>
           <div className="mt-6 flex space-x-4">
             <a
